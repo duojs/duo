e5f5e3f11b21e678fa059e11ec0b3e01493e837e
Amir Abu Shareb <yields@icloud.com>
2014-05-11 15:41:12 +0300

    duo-build(1): add --entry option

---
+++ example/tests.js
@@ -0,0 +1,576 @@
+(function outer(modules, cache, entry) {var previousRequire = typeof require == "function" && require;function newRequire(name, jumped){if(!cache[name]) {if(!modules[name]) {var currentRequire = typeof require == "function" && require;if (!jumped && currentRequire) return currentRequire(name, true);if (previousRequire) return previousRequire(name, true);throw new Error('Cannot find module \'' + name + '\'');}var m = cache[name] = {exports:{}};modules[name][0].call(m.exports, function(x){var id = modules[name][1][x];return newRequire(id ? id : x);},m,m.exports,outer,modules,cache,entry);}return cache[name].exports;}for(var i=0;i<entry.length;i++) newRequire(entry[i]);return newRequire;})({
+1: [function(require, module, exports) {
+
+
+
+module.exports = os();
+
+function os() {
+  var ua = navigator.userAgent;
+  if (/mac/i.test(ua)) return 'mac';
+  if (/win/i.test(ua)) return 'windows';
+  if (/linux/i.test(ua)) return 'linux';
+}
+
+}, {}],
+
+2: [function(require, module, exports) {
+
+
+/**
+ * map
+ */
+
+var map = {
+    backspace: 8
+  , tab: 9
+  , clear: 12
+  , enter: 13
+  , shift: 16
+  , ctrl: 17
+  , alt: 18
+  , capslock: 20
+  , escape: 27
+  , esc: 27
+  , space: 32
+  , left: 37
+  , up: 38
+  , right: 39
+  , down: 40
+  , del: 46
+  , comma: 188
+  , ',': 188
+  , '.': 190
+  , '/': 191
+  , '`': 192
+  , '-': 189
+  , '=': 187
+  , ';': 186
+  , '[': 219
+  , '\\': 220
+  , ']': 221
+  , '\'': 222
+};
+
+/**
+ * find a keycode.
+ *
+ * @param {String} name
+ * @return {Number}
+ */
+
+module.exports = function(name){
+  return map[name] || name.toUpperCase().charCodeAt(0);
+};
+
+}, {}],
+
+3: [function(require, module, exports) {
+
+
+/**
+ * dependencies
+ */
+
+var keycode = require('keycode');
+
+/**
+ * Export `sequence`
+ */
+
+module.exports = sequence;
+
+/**
+ * Create sequence fn with `keys`.
+ * optional `ms` which defaults
+ * to `500ms` and `fn`.
+ *
+ * Example:
+ *
+ *      seq = sequence('a b c', fn);
+ *      el.addEventListener('keydown', seq);
+ *
+ * @param {String} keys
+ * @param {Number} ms
+ * @param {Function} fn
+ * @return {Function}
+ * @api public
+ */
+
+function sequence(keys, ms, fn){
+  var codes = keys.split(/ +/).map(keycode)
+    , clen = codes.length
+    , seq = []
+    , i = 0
+    , prev;
+
+  if (2 == arguments.length) {
+    fn = ms;
+    ms = 500;
+  }
+
+  return function(e){
+    var code = codes[i++];
+    if (42 != code && code != e.which) return reset();
+    if (prev && new Date - prev > ms) return reset();
+    var len = seq.push(e.which);
+    prev = new Date;
+    if (len != clen) return;
+    reset();
+    fn(e);
+  };
+
+  function reset(){
+    prev = null;
+    seq = [];
+    i = 0;
+  }
+};
+
+}, {"keycode":2}],
+
+4: [function(require, module, exports) {
+
+
+/**
+ * Slice reference.
+ */
+
+var slice = [].slice;
+
+/**
+ * Bind `obj` to `fn`.
+ *
+ * @param {Object} obj
+ * @param {Function|String} fn or string
+ * @return {Function}
+ * @api public
+ */
+
+module.exports = function(obj, fn){
+  if ('string' == typeof fn) fn = obj[fn];
+  if ('function' != typeof fn) throw new Error('bind() requires a function');
+  var args = [].slice.call(arguments, 2);
+  return function(){
+    return fn.apply(obj, args.concat(slice.call(arguments)));
+  }
+};
+
+}, {}],
+
+5: [function(require, module, exports) {
+
+
+/**
+ * dependencies
+ */
+
+var sequence = require('k-sequence')
+  , keycode = require('keycode')
+  , event = require('event')
+  , os = require('os');
+
+/**
+ * modifiers.
+ */
+
+var modifiers = {
+  224: 'command',
+  91: 'command',
+  93: 'command',
+  16: 'shift',
+  17: 'ctrl',
+  18: 'alt'
+};
+
+/**
+ * Super key.
+ */
+
+exports.super = 'mac' == os
+  ? 'command'
+  : 'ctrl';
+
+/**
+ * Handle the given `KeyboardEvent` or bind
+ * a new `keys` handler.
+ *
+ * @param {String|KeyboardEvent} e
+ * @param {Function} fn
+ * @api private
+ */
+
+exports.handle = function(e, fn){
+  var ignore = this.ignore;
+  var event = e.type;
+  var code = e.which;
+
+  // bind
+  if (fn) return this.bind(e, fn);
+
+  // modifiers
+  var mod = modifiers[code];
+  if ('keydown' == event && mod) {
+    this.super = exports.super == mod;
+    this[mod] = true;
+    this.modifiers = true;
+    return;
+  }
+
+  // ignore
+  if (ignore && ignore(e)) return;
+
+  // listeners
+  var all = this.listeners;
+
+  // match
+  for (var i = 0; i < all.length; ++i) {
+    var invoke = true;
+    var obj = all[i];
+    var seq = obj.seq;
+    var mods = obj.mods;
+    var fn = seq || obj.fn;
+
+    if (!seq && code != obj.code) continue;
+    if (event != obj.event) continue;
+
+    for (var j = 0; j < mods.length; ++j) {
+      if (!this[mods[j]]) {
+        invoke = null;
+        break;
+      }
+    }
+
+    invoke && fn(e);
+  }
+};
+
+/**
+ * Destroy this `k` dispatcher instance.
+ *
+ * @api public
+ */
+
+exports.destroy = function(){
+  event.unbind(this.el, 'keydown', this._handle);
+  event.unbind(this.el, 'keyup', this._handle);
+  event.unbind(this.el, 'keyup', this._clear);
+  event.unbind(this.el, 'focus', this._clear);
+  this.listeners = [];
+};
+
+/**
+ * Unbind the given `keys` with optional `fn`.
+ *
+ * example:
+ *
+ *      k.unbind('enter, tab', myListener); // unbind `myListener` from `enter, tab` keys
+ *      k.unbind('enter, tab'); // unbind all `enter, tab` listeners
+ *      k.unbind(); // unbind all listeners
+ *
+ * @param {String} keys
+ * @param {Function} fn
+ * @return {k}
+ * @api public
+ */
+
+exports.unbind = function(keys, fn){
+  var fns = this.listeners
+    , len = fns.length
+    , all;
+
+  // unbind all
+  if (0 == arguments.length) {
+    this.listeners = [];
+    return this;
+  }
+
+  // parse
+  all = parseKeys(keys);
+
+  // unbind
+  for (var i = 0; i < all.length; ++i) {
+    for (var j = 0, obj; j < len; ++j) {
+      obj = fns[j];
+      if (!obj) continue;
+      if (fn && obj.fn != fn) continue;
+      if (obj.key != all[i].key) continue;
+      if (!matches(obj, all[i])) continue;
+      fns.splice(j--, 1);
+    }
+  }
+
+  return this;
+};
+
+/**
+ * Bind the given `keys` to `fn` with optional `event`
+ *
+ * example:
+ *
+ *      k.bind('shift + tab, ctrl + a', function(e){});
+ *
+ * @param {String} event
+ * @param {String} keys
+ * @param {Function} fn
+ * @return {k}
+ * @api public
+ */
+
+exports.bind = function(event, keys, fn){
+  var fns = this.listeners
+    , len
+    , all;
+
+  if (2 == arguments.length) {
+    fn = keys;
+    keys = event;
+    event = 'keydown';
+  }
+
+  all = parseKeys(keys);
+  len = all.length;
+
+  for (var i = 0; i < len; ++i) {
+    var obj = all[i];
+    obj.seq = obj.seq && sequence(obj.key, fn);
+    obj.event = event;
+    obj.fn = fn;
+    fns.push(obj);
+  }
+
+  return this;
+};
+
+/**
+ * Bind keyup with `keys` and `fn`.
+ *
+ * @param {String} keys
+ * @param {Function} fn
+ * @return {k}
+ * @api public
+ */
+
+exports.up = function(keys, fn){
+  return this.bind('keyup', keys, fn);
+};
+
+/**
+ * Bind keydown with `keys` and `fn`.
+ *
+ * @param {String} keys
+ * @param {Function} fn
+ * @return {k}
+ * @api public
+ */
+
+exports.down = function(keys, fn){
+  return this.bind('keydown', keys, fn);
+};
+
+/**
+ * Clear all modifiers on `keyup`.
+ *
+ * @api private
+ */
+
+exports.clear = function(e){
+  var code = e.keyCode || e.which;
+  if (!(code in modifiers)) return;
+  this[modifiers[code]] = null;
+  this.modifiers = this.command
+    || this.shift
+    || this.ctrl
+    || this.alt;
+};
+
+/**
+ * Ignore all input elements by default.
+ *
+ * @param {Event} e
+ * @return {Boolean}
+ * @api private
+ */
+
+exports.ignore = function(e){
+  var el = e.target || e.srcElement;
+  var name = el.tagName.toLowerCase();
+  return 'textarea' == name
+    || 'select' == name
+    || 'input' == name;
+};
+
+/**
+ * Parse the given `keys`.
+ *
+ * @param {String} keys
+ * @return {Array}
+ * @api private
+ */
+
+function parseKeys(keys){
+  keys = keys.replace('super', exports.super);
+
+  var all = ',' != keys
+    ? keys.split(/ *, */)
+    : [','];
+
+  var ret = [];
+  for (var i = 0; i < all.length; ++i) {
+    if ('' == all[i]) continue;
+    var mods = all[i].split(/ *\+ */);
+    var key = mods.pop() || ',';
+
+    ret.push({
+      seq: !!~ key.indexOf(' '),
+      code: keycode(key),
+      mods: mods,
+      key: key
+    });
+  }
+
+  return ret;
+}
+
+/**
+ * Check if the given `a` matches `b`.
+ *
+ * @param {Object} a
+ * @param {Object} b
+ * @return {Boolean}
+ * @api private
+ */
+
+function matches(a, b){
+  return 0 == b.mods.length || eql(a, b);
+}
+
+/**
+ * Shallow eql util.
+ *
+ * TODO: move to yields/eql
+ *
+ * @param {Array} a
+ * @param {Array} b
+ * @return {Boolean}
+ * @api private
+ */
+
+function eql(a, b){
+  a = a.mods.sort().toString();
+  b = b.mods.sort().toString();
+  return a == b;
+}
+
+}, {"k-sequence":3,"keycode":2,"event":6,"os":1}],
+
+6: [function(require, module, exports) {
+
+var bind = window.addEventListener ? 'addEventListener' : 'attachEvent',
+    unbind = window.removeEventListener ? 'removeEventListener' : 'detachEvent',
+    prefix = bind !== 'addEventListener' ? 'on' : '';
+
+/**
+ * Bind `el` event `type` to `fn`.
+ *
+ * @param {Element} el
+ * @param {String} type
+ * @param {Function} fn
+ * @param {Boolean} capture
+ * @return {Function}
+ * @api public
+ */
+
+exports.bind = function(el, type, fn, capture){
+  el[bind](prefix + type, fn, capture || false);
+  return fn;
+};
+
+/**
+ * Unbind `el` event `type`'s callback `fn`.
+ *
+ * @param {Element} el
+ * @param {String} type
+ * @param {Function} fn
+ * @param {Boolean} capture
+ * @return {Function}
+ * @api public
+ */
+
+exports.unbind = function(el, type, fn, capture){
+  el[unbind](prefix + type, fn, capture || false);
+  return fn;
+};
+}, {}],
+
+7: [function(require, module, exports) {
+
+
+/**
+ * dependencies.
+ */
+
+var event = require('event')
+  , proto = require('./proto')
+  , bind = require('bind');
+
+/**
+ * Create a new dispatcher with `el`.
+ *
+ * example:
+ *
+ *      var k = require('k')(window);
+ *      k('shift + tab', function(){});
+ *
+ * @param {Element} el
+ * @return {Function}
+ * @api public
+ */
+
+module.exports = function(el){
+  function k(e, fn){ k.handle(e, fn) };
+  k._handle = bind(k, proto.handle);
+  k._clear = bind(k, proto.clear);
+  event.bind(el, 'keydown', k._handle, false);
+  event.bind(el, 'keyup', k._handle, false);
+  event.bind(el, 'keyup', k._clear, false);
+  event.bind(el, 'focus', k._clear, false);
+  for (var p in proto) k[p] = proto[p];
+  k.listeners = [];
+  k.el = el;
+  return k;
+};
+
+}, {"event":6,"./proto":5,"bind":4}],
+
+8: [function(require, module, exports) {
+
+
+/**
+ * Module dependencies.
+ */
+
+var k = require('k');
+
+/**
+ * Expose `App`
+ */
+
+module.exports = App;
+
+/**
+ * Initialize `App`
+ */
+
+function App(){
+  if (!(this instanceof App)) return new App;
+}
+
+}, {"k":7}],
+
+9: [function(require, module, exports) {
+
+
+require('../app');
+
+}, {"../app":8}]}, {}, [9])
