5750831af4de581e786336260443994607e19128
Matthew Mueller <mattmuelle@gmail.com>
2014-06-12 03:27:08 +0530

    ensure path resolution is within root

--- test/duo.js
+++ test/duo.js
@@ -33,6 +33,18 @@ describe('Duo', function(){
     assert('resolved' == ctx.main);
   })
 
+  it('should prevent requires from requiring higher than root', function*() {
+    try {
+      var js = yield build('unsafe').run();
+    } catch (e) {
+      // TODO: ensure correct error message,
+      // better error isn't passing that through
+      return;
+    }
+
+    assert(false);
+  })
+
   it('should fetch and build direct dependencies', function*(){
     var js = yield build('simple-deps').run();
     var ctx = evaluate(js);
