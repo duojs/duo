199caa014a3f282daf34fd71942eab18b6fbea6e
Stephen Mathieson <smath23@gmail.com>
2014-08-27 09:20:35 -0400

    test: Verify examples still work

---
+++ test/examples.js
@@ -0,0 +1,41 @@
+
+var path = require('path');
+var join = path.join;
+var fs = require('fs');
+var ls = fs.readdirSync;
+var exists = fs.existsSync;
+var fmt = require('util').format;
+var spawn = require('child_process').spawn;
+var assert = require('assert');
+
+var gnode = require.resolve('gnode/bin/gnode');
+var dir = join(__dirname, '..', 'examples');
+
+/**
+ * Built files.
+ */
+
+var map = {
+  css: 'build/build.css',
+  default: 'build.js',
+};
+
+ls(dir).forEach(function (example) {
+  var root = join(dir, example);
+  var index = join(root, 'index.js');
+  if (!exists(index)) return;
+
+  describe(fmt('node examples/%s/index.js', example), function () {
+    this.timeout('10s');
+    it('should build', function (done) {
+      var args = [gnode, index];
+      var proc = spawn('node', args);
+      var build = join(root, map[example] || map.default);
+      proc.on('close', function (code) {
+        assert(0 == code);
+        assert(exists(build));
+        done();
+      });
+    });
+  });
+});
